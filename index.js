028358e9664fad56843fd280b6bc5dea6b1f74c7
qurihara <qurihara@unryu.org>
2020-05-17 18:08:54 +0900

    ok

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ const parser = csv.parse((error, data) => {
     data.forEach((element, index, array) => {
         let row = [];
         //debug
-        row.push(element[8]);//date
-        row.push(element[9]);//time
+        // row.push(element[8]);//date
+        // row.push(element[9]);//time
         let date = new Date(element[8] + ' ' + element[9] + ' GMT');
-        row.push(date);//time
+        // row.push(date);//time
 
         row.push(date.getTime());//time
         row.push(element[12]);//lat
@@ -29,19 +29,55 @@ const parser = csv.parse((error, data) => {
         newData.push(row);
     })
 
-    console.log('処理データ');
-    console.log(newData);
+    // console.log('処理データ');
+    // console.log(newData);
 
     fs.createReadStream('master.csv').pipe(parser2);
 
 })
 
-const parser2 = csv.parse((error, data) => {
+const parser2 = csv.parse({ from_line: 2 },(error, data) => {
   data.forEach((element, index, array) => {
-    let now = new Date(element[7]);
-    console.log(now);
-    let lat = element[8];//lat
-    let lon = element[8];//lon
+    let s = element[8].split('-');
+    let now = s[0] + s[1] + ' GMT';
+    let dnow = new Date(now);
+    // console.log(now);
+    // console.log(dnow);
+    let t = dnow.getTime();
+
+    //debug
+    let lat = element[9];//lat
+    let lon = element[10];//lon
+
+    console.log(t + ',' + lat + ',' + lon);
+
+    let closest = [t,lat,lon];
+    newData.some((element, index, array) => {
+        // console.log(t + ',' +element[0] + '\n' )
+        if (element[0] >= t){
+          // console.log("scanning end")
+          let last = element;
+          let a = element[0] - t;
+          let b = t - closest[0];
+          if (a > b) {
+            // do nothing
+          }else{
+            closest = last;
+          }
+
+          return true;
+        }
+        closest = element;
+    })
+    // use closest
+    // console.log(closest);
+    let kenzan = Math.abs(lat - closest[1]) + Math.abs(lon - closest[2]);
+    console.log(kenzan);
+    if (kenzan > 0) {
+      console.log("error");
+      process.exit(1);
+    }
+
   })
   //write
   csv.stringify(newData,(error,output)=>{
