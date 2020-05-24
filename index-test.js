bdc97c86ce594d766a6a3553efa8bb66b82a746d
qurihara <qurihara@unryu.org>
2020-05-24 09:56:49 +0900

    ok

--- index-test.js
+++ index-test.js
@@ -1,18 +1,11 @@
 const fs = require('fs');
 const csv = require('csv');
 
-//filepath
-var gps_path = "./dat/93/gps.csv";
-var work_path = "./dat/93/work.csv";
-var out_path =  "./dat/93/result.csv";
-
 //変換後の配列を格納
 var newData = [];
 
-var outData = [];
-
 //処理（跡でpipeに食べさせる）
-const parser = csv.parse({ from_line: 2 },(error, data) => {
+const parser = csv.parse((error, data) => {
 
     //内容出力
     // console.log('初期データ');
@@ -25,18 +18,13 @@ const parser = csv.parse({ from_line: 2 },(error, data) => {
         //debug
         // row.push(element[8]);//date
         // row.push(element[9]);//time
-        // let date = new Date(element[8] + ' ' + element[9] + ' GMT');
-        let date = new Date(element[0] + ' ' + element[1] + ' GMT');
+        let date = new Date(element[8] + ' ' + element[9] + ' GMT');
         // row.push(date);//time
 
         row.push(date.getTime());//time
-        // row.push(element[12]);//lat
-        // row.push(element[13]);//lon
-        row.push(element[2]);//lat
-        row.push(element[3]);//lon
-        // row.push(date);//time
-        row.push(element[0]);
-        row.push(element[1]);
+        row.push(element[12]);//lat
+        row.push(element[13]);//lon
+        row.push(date);//time
 
         //新たに1行分の配列(row)を作成し、新配列(newData)に追加。
         newData.push(row);
@@ -45,27 +33,28 @@ const parser = csv.parse({ from_line: 2 },(error, data) => {
     // console.log('処理データ');
     // console.log(newData);
 
-    fs.createReadStream(work_path).pipe(parser2);
+    fs.createReadStream('master.csv').pipe(parser2);
 
 })
 
 const parser2 = csv.parse({ from_line: 2 },(error, data) => {
   let count = 0;
-  data.forEach((element2, index, array) => {
+  data.forEach((element, index, array) => {
     count++;
-    let s = element2[0].split('-');
+    let s = element[8].split('-');
     let now = s[0] + s[1] + ' GMT';
     let dnow = new Date(now);
     // console.log(now);
     // console.log(dnow);
     let t = dnow.getTime();
 
-    // debug
-    // let lat = element[9];//lat
-    // let lon = element[10];//lon
-    // console.log(t + ',' + lat + ',' + lon);
+    //debug
+    let lat = element[9];//lat
+    let lon = element[10];//lon
 
-    let closest = [0,0,0]//,lat,lon];
+    console.log(t + ',' + lat + ',' + lon);
+
+    let closest = [t,lat,lon];
     newData.some((element, index, array) => {
         // console.log(t + ',' +element[0] + '\n' )
         if (element[0] >= t){
@@ -85,13 +74,12 @@ const parser2 = csv.parse({ from_line: 2 },(error, data) => {
     })
     // use closest
     // console.log(closest);
-    // let kenzan = Math.abs(lat - closest[1]) + Math.abs(lon - closest[2]);
-    // console.log( count + ',' +kenzan);
-    // if (kenzan > 0) {
-    //   console.log("error");
-    //   // process.exit(1);
-    // }
-    outData.push([element2[0], closest[3],closest[4],closest[1],closest[2]]);
+    let kenzan = Math.abs(lat - closest[1]) + Math.abs(lon - closest[2]);
+    console.log( count + ',' +kenzan);
+    if (kenzan > 0) {
+      console.log("error");
+      // process.exit(1);
+    }
 
   })
   //write
@@ -100,15 +88,7 @@ const parser2 = csv.parse({ from_line: 2 },(error, data) => {
           console.log('処理データをCSV出力しました。');
       })
   })
-
-  //write
-  csv.stringify(outData,(error,output)=>{
-      fs.writeFile(out_path,output,(error)=>{
-          console.log('処理データをCSV出力しました。');
-      })
-  })
-
 })
 
 //読み込みと処理を実行
-fs.createReadStream(gps_path).pipe(parser);
+fs.createReadStream('data.csv').pipe(parser);
